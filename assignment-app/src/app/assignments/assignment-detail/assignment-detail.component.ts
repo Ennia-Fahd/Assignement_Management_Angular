91251f0e5fa23f42ca7ca4c9d52c0c496c9df3ca
Ennia-Fahd <enniafahd@gmail.com>
2023-11-22 09:02:00 +0100

    Page 103 -- add checkbox

--- assignment-app/src/app/assignments/assignment-detail/assignment-detail.component.ts
+++ assignment-app/src/app/assignments/assignment-detail/assignment-detail.component.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import { Input } from '@angular/core';
 import { Assignment } from '../assignment.model';
 import {MatCardModule}  from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-assignment-detail',
   standalone: true,
   imports: [CommonModule,
-            MatCardModule],
+            MatCardModule,
+            MatCheckboxModule],
   templateUrl: './assignment-detail.component.html',
   styleUrl: './assignment-detail.component.css'
 })
 export class AssignmentDetailComponent {
   @Input() assignmentTransmis !: Assignment;
+
+  onAssignmentRendu() {
+    this.assignmentTransmis.rendu = true;
+  }
 }
